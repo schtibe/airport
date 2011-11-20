cf378b5b0bb5201a4b694c2d82975d02055fdde0
Stefan Heinemann <heins4@bfh.ch>
2011-11-20 18:44:36 +0100

    Little painting test with qt

--- airport/src/air/QtTest.java
+++ airport/src/air/QtTest.java
@@ -1,14 +1,19 @@
 package air;
 
-
-import com.trolltech.qt.gui.*;
+import com.trolltech.qt.gui.QApplication;
+import com.trolltech.qt.gui.QGraphicsRectItem;
+import com.trolltech.qt.gui.QGraphicsScene;
+import com.trolltech.qt.gui.QGraphicsView;
 
 public class QtTest {
 	  public static void main(String args[]) {
 	        QApplication.initialize(args);
 	 
-	        QPushButton hello = new QPushButton("Hello World!");
-	        hello.show();
+	        QGraphicsScene scene = new QGraphicsScene();
+	        QGraphicsRectItem rect = scene.addRect(0,0,100,100);
+	        QGraphicsView view = new QGraphicsView(scene);
+	        view.show();
+	        
 	 
 	        QApplication.exec();
 	    }
