ec929019a917f07a9b25dae9895ecdd0e558ed17
Stefan Heinemann <heins4@bfh.ch>
2011-12-05 16:43:15 +0100

    Made the mpi stuff working

--- airport/run-mpi.sh
+++ airport/run-mpi.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+USER=`whoami`
+QT_PATH=$(echo $(ls -d /tmp/QtJambi_*/lib))
+
+cd $QT_PATH
+chrpath -d *.so*
+cd -
+
 BASEDIR="$(pwd)/$(dirname $0)"
 export P2PMPI_HOME="${BASEDIR}/lib/p2pmpi"
 export PATH="${BASEDIR}/lib/p2pmpi/bin:${PATH}"
@@ -8,4 +15,7 @@ export CLASSPATH="${CLASSPATH}:${BASEDIR}/lib/qtjambi-4.7.0.jar"
 export CLASSPATH="${CLASSPATH}:${BASEDIR}/qtjambi-linux64-gcc-4.7.0.jar"
 export CLASSPATH="${CLASSPATH}:${BASEDIR}/qtjambi-util-4.7.0.jar"
 export CLASSPATH="${CLASSPATH}:${BASEDIR}/qtjambi-linux32-gcc-4.7.0.jar"
+#export LD_LIBRARY_PATH="/tmp/QtJambi_${USER}_amd64_4.7.0_gcc-20100927-1542/lib/:${LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${QT_PATH}:${LD_LIBRARY_PATH}"
 "${BASEDIR}/lib/p2pmpi/bin/p2pmpirun" -n 3  air.Simulator
+
