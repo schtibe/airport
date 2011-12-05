db901bd9ad584185ecdf40cf18ce594f4bf95cdc
Stefan Heinemann <heins4@bfh.ch>
2011-12-05 18:16:12 +0100

    Made the processor switch in the run-mpi

--- airport/run-mpi.sh
+++ airport/run-mpi.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 USER=`whoami`
-QT_PATH=$(echo $(ls -d /tmp/QtJambi_*/lib))
 
-cd $QT_PATH
-chrpath -d *.so*
-cd -
+
+if [[ `uname -p` == 'x86_64' ]]; then
+	export processor=64
+else
+	export processor=32
+fi
 
 BASEDIR="$(pwd)/$(dirname $0)"
 export P2PMPI_HOME="${BASEDIR}/lib/p2pmpi"
@@ -16,6 +18,7 @@ export CLASSPATH="${CLASSPATH}:${BASEDIR}/qtjambi-linux64-gcc-4.7.0.jar"
 export CLASSPATH="${CLASSPATH}:${BASEDIR}/qtjambi-util-4.7.0.jar"
 export CLASSPATH="${CLASSPATH}:${BASEDIR}/qtjambi-linux32-gcc-4.7.0.jar"
 #export LD_LIBRARY_PATH="/tmp/QtJambi_${USER}_amd64_4.7.0_gcc-20100927-1542/lib/:${LD_LIBRARY_PATH}"
-export LD_LIBRARY_PATH="${QT_PATH}:${LD_LIBRARY_PATH}"
+#export LD_LIBRARY_PATH="${QT_PATH}:${LD_LIBRARY_PATH}"
+export  LD_LIBRARY_PATH="lib/qt-native-${processor}/lib:${LD_LIBRARY_PATH}"
 "${BASEDIR}/lib/p2pmpi/bin/p2pmpirun" -n 3  air.Simulator
 
