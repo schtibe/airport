f6e67c3398f71402fc5080be5e4a0a0005106cd7
Stefan Heinemann <heins4@bfh.ch>
2012-01-19 12:06:18 +0100

    Implemented the lookahead queue

---
+++ airport/src/air/LookaheadQueue.java
@@ -0,0 +1,71 @@
+package air;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import p2pmpi.mpi.MPI;
+
+public class LookaheadQueue {
+	List<Long> queues[];
+	int LPcount = 0;
+	
+	public LookaheadQueue() {
+		this.LPcount = MPI.COMM_WORLD.SizeTotal();
+		
+		this.queues = new ArrayList[this.LPcount];
+		for (int i = 0; i < this.LPcount; i++) {
+			this.queues[i] = new ArrayList<Long>();
+		}
+	}
+	
+	/**
+	 * Add lookahead to the corresponding queue
+	 * 
+	 * @param sender Who sent the lookahead 
+	 * @param timestamp The lookahead value
+	 */
+	public void addLookahead(int sender, Long timestamp) {
+		this.queues[sender].add(timestamp);
+		Collections.sort(this.queues[sender]);
+	}
+	
+	/**
+	 * Return the smallest lookahead
+	 * 
+	 * If one of the queues is empty, return null instead
+	 * so the simulation knows not to proceed, because
+	 * all queues have to have some value, otherwise it's not
+	 * safe to proceed.
+	 * The queues are sorted, so we are sure when we take
+	 * the 0-position value we take the smallest from the queue
+	 * to check which one's the smallest
+	 */
+	public Long getSmallestLookahead() {
+		for (int i = 0; i < this.LPcount; i++) {
+			if (this.queues[i].size() == 0) {
+				return null;
+			} 
+		}
+		
+		// we are now sure we can do this
+		Long smallestLookahead = this.queues[0].get(0);
+		for (int i = 1; i < this.LPcount; i++) {
+			smallestLookahead = Math.min(this.queues[i].get(0), smallestLookahead);
+		}
+		
+		return smallestLookahead;
+	}
+	
+	/**
+	 * Remove the lookahead
+	 * 
+	 * Remove the lookahead from all the queues (in case
+	 * the lookahead is present in several queues)
+	 */
+	public void removeLookeahead(Long lookahead) {
+		for (int i = 0; i < this.LPcount; i++) {
+			this.queues[i].remove(lookahead);
+		}
+	}
+}
